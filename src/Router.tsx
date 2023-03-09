70b0b98bf87e37ed41f18b0325837bceca9bd286
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-03-09 18:49:15 +0300

    import new screens.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import StepOne from './Screens/Onboarding/StepOne';
 import StepTwo from './Screens/Onboarding/StepTwo';
 import StepThree from './Screens/Onboarding/StepThree';
+
 import AccountInformation from './Screens/SignUp/AccountInformation';
+import PersonalInformation from './Screens/SignUp/PersonalInformation';
+import SignUpSuccess from './Screens/SignUp/SignUpSuccess';
+
+import SignIn from './Screens/SignIn';
+
+import ResetPassword from './Screens/ForgotPassword/ResetPassword';
+import VerifyAccount from './Screens/ForgotPassword/VerifyAccount';
+import ForgotPasswordSuccess from './Screens/ForgotPassword/ForgotPasswordSuccess';
 
 const Stack = createNativeStackNavigator();
 
 const Router = () => {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#fff" />
       <Stack.Navigator>
         <Stack.Screen
           name="StepOneScreen"
@@ -34,8 +46,50 @@ const Router = () => {
           }}
         />
         <Stack.Screen
-          name="SignUpScreen"
+          name="AccountInformationScreen"
           component={AccountInformation}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="PersonalInformationScreen"
+          component={PersonalInformation}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="SignUpSuccessScreen"
+          component={SignUpSuccess}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="SignInScreen"
+          component={SignIn}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ResetPasswordScreen"
+          component={ResetPassword}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="VerifyAccountScreen"
+          component={VerifyAccount}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ForgotPasswordSuccessScreen"
+          component={ForgotPasswordSuccess}
           options={{
             headerShown: false,
           }}
